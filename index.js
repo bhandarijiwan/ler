f3409b540ed4119f0373e186cfbab8a45f60ed97
Jiwan Bhandari <jiwanbhandari@Jiwans-MacBook-Pro.local>
2022-04-10 20:24:11 -0700

    stuff

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 import './peg-0.10.0.js';
 
 //const grammar_nquery = await Deno.readTextFile('./sql.pegjs')
-const grammar = await Deno.readTextFile('./e.pegjs');
+const grammar1 = await Deno.readTextFile('./e.pegjs');
 
-
-const options = {
-    output: "source",
+function parserGen() {
+    const options = {
+        output: "source",
+    }
+    try {
+        const parser = peg.generate(grammar1, options);
+        console.log("👍")
+    } catch (e) {
+        console.error(e)
+    }
 }
+parserGen()
 
-try {
-
-    const parser = peg.generate(grammar, options);
-} catch (e) {
-    console.error(e)
+function debounce(time, f) {
+    let inProgress = false;
+    return () => {
+        if (!inProgress) {
+            inProgress = true;
+            setTimeout(() => {
+                f()
+                inProgress = false;
+            }, time)
+        }
+    }
+}
+const debouncedParseGen = debounce(1000, parserGen)
+const watcher = Deno.watchFs('./e.pegjs')
+for await (const event of watcher) {
+    debouncedParseGen()
 }
 
 
